902ab88186328d4bf0e3e2abce65faff9e255926
PhilipMay <philip@may.la>
2021-07-25 16:10:48 +0200

    add code

---
+++ ml_cloud_tools/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2021 Philip May
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+"""ML-Cloud-Tools main package."""
+
+from ml_cloud_tools.version import __version__
+
+
+__all__ = ["__version__"]
---
+++ ml_cloud_tools/s3.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2021 Philip May
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+
+"""S3 tools."""
