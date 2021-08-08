ed78da9550d75fd6a6f206a6dafadf96ae93ca34
PhilipMay <philip@may.la>
2021-08-08 07:52:37 +0200

    add list_s3_files test

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -8,7 +8,12 @@ from pathlib import Path
 import boto3
 from moto import mock_s3
 
-from ml_cloud_tools import copy_file_to_s3_file, copy_s3_dir_to_dir, copy_s3_file_to_file
+from ml_cloud_tools import (
+    copy_file_to_s3_file,
+    copy_s3_dir_to_dir,
+    copy_s3_file_to_file,
+    list_s3_files,
+)
 
 
 @mock_s3
@@ -97,3 +102,30 @@ def test_copy_s3_dir_to_dir(tmpdir):
     ) as f:
         locale_content = f.read()
     assert locale_content == file_content
+
+
+@mock_s3
+def test_list_s3_files(tmpdir):
+    bucket_name = "moto_mock_bucket"
+    file_name = "s3_test_file.txt"
+    s3_root_dir = (Path("test_dir") / Path("a")).as_posix()
+    s3_dir_a_x = (Path(s3_root_dir) / Path("x")).as_posix()
+    s3_dir_a_y = (Path(s3_root_dir) / Path("y")).as_posix()
+    full_s3_file_name_a_x = (Path(s3_dir_a_x) / Path(file_name)).as_posix()
+    full_s3_file_name_a_y = (Path(s3_dir_a_y) / Path(file_name)).as_posix()
+    file_content = "This is a s3 test file."
+
+    # create mock bucket and s3 file
+    s3_resource = boto3.resource("s3")
+    s3_resource.create_bucket(Bucket=bucket_name)
+
+    # create s3 file
+    s3_client = boto3.client("s3")
+    s3_client.put_object(Bucket=bucket_name, Key=full_s3_file_name_a_x, Body=file_content)
+    s3_client.put_object(Bucket=bucket_name, Key=full_s3_file_name_a_y, Body=file_content)
+
+    s3_file_list = list_s3_files(s3_root_dir, bucket_name)
+
+    assert len(s3_file_list) == 2
+    assert (Path(s3_root_dir) / Path("x") / Path(file_name)).as_posix() in s3_file_list
+    assert (Path(s3_root_dir) / Path("y") / Path(file_name)).as_posix() in s3_file_list
