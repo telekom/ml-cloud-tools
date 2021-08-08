2569f87365c1142f9091c2f40f2d09879354afdb
PhilipMay <philip@may.la>
2021-08-08 07:01:54 +0200

    add all func. to __init__.py

--- ml_cloud_tools/__init__.py
+++ ml_cloud_tools/__init__.py
@@ -4,8 +4,24 @@
 
 """ML-Cloud-Tools main package."""
 
+from ml_cloud_tools.s3 import (
+    copy_dir_to_s3_dir,
+    copy_file_to_s3_file,
+    copy_s3_dir_to_dir,
+    copy_s3_file_to_file,
+    list_s3_files,
+)
+
+
 # Versioning follows the `Semantic Versioning Specification <https://semver.org/>`__ and
 # `PEP 440 -- Version Identification and Dependency Specification <https://www.python.org/dev/peps/pep-0440/>`__.  # noqa: E501
 __version__ = "0.0.1rc5"
 
-__all__ = ["__version__"]
+__all__ = [
+    "copy_dir_to_s3_dir",
+    "copy_file_to_s3_file",
+    "copy_s3_dir_to_dir",
+    "copy_s3_file_to_file",
+    "list_s3_files",
+    "__version__",
+]
